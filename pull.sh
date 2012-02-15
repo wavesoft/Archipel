c543ddf4d9f06dd6a668b71061029dcf06378bc4
wavesoft <johnys2@gmail.com>
2012-02-15 15:26:44 +0100

    Changed the pull script

--- pull.sh
+++ pull.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo " * fetching from upstream"
+git fetch upstream
+git merge upstream/master
+
 echo " * updating all submodules"
 git submodule update
 git submodule foreach git submodule update
